274d04e7b9de72fe96d1983ebf9c172966bdc208
YuBaoku <49938469+EmmonsCurse@users.noreply.github.com>
2024-01-16 19:35:35 +0800

    set enable_new_executor only in gpu

--- inference/python_api_test/test_case/infer_test.py
+++ inference/python_api_test/test_case/infer_test.py
@@ -117,7 +117,6 @@ class InferenceTest(object):
             self.pd_config.disable_gpu()
         elif device == "gpu":
             self.pd_config.enable_use_gpu(256, 0)
-        self.pd_config.enable_new_executor()
         self.pd_config.enable_memory_optim()
         self.pd_config.collect_shape_range_info(f"{model_path}/shape_range.pbtxt")
         predictor = paddle_infer.create_predictor(self.pd_config)
@@ -300,7 +299,6 @@ class InferenceTest(object):
         if precision == "int8":
             self.pd_config.enable_mkldnn_int8()
         self.pd_config.set_mkldnn_cache_capacity(mkldnn_cache_capacity)
-        self.pd_config.enable_new_executor()
         predictor = paddle_infer.create_predictor(self.pd_config)
 
         input_names = predictor.get_input_names()
@@ -528,8 +526,6 @@ class InferenceTest(object):
             for ir_pass in delete_pass_list:
                 self.pd_config.delete_pass(ir_pass)
 
-        self.pd_config.enable_new_executor()
-
         predictor = paddle_infer.create_predictor(self.pd_config)
 
         input_names = predictor.get_input_names()
@@ -662,7 +658,6 @@ class InferenceTest(object):
         if delete_op_list:
             self.pd_config.exp_disable_tensorrt_ops(delete_op_list)
 
-        self.pd_config.enable_new_executor()
         predictor = paddle_infer.create_predictor(self.pd_config)
 
         input_names = predictor.get_input_names()
@@ -746,7 +741,6 @@ class InferenceTest(object):
             {names[i]: max_input_shape[i] for i in range(len(names))},
             {names[i]: opt_input_shape[i] for i in range(len(names))},
         )
-        self.pd_config.enable_new_executor()
 
         predictor = paddle_infer.create_predictor(self.pd_config)
 
